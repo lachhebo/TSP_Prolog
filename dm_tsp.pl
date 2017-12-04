d91016d424f90613ad5ba28ac1ddd94becb628e2
lachhebo <ismaellachheb@gmail.com>
2017-12-04 11:10:42 +0100

    Initialisation

---
+++ dm_tsp.pl
@@ -0,0 +1,25 @@
+/* Lachheb Ismaël
+   Quentin Bresson
+
+   Voyageur du commerce
+    - Avec méthodes gloutonnes
+    - Avec méthode de l'élastique
+
+*/
+
+/* ****************** Méthode Gloutonne *********************** */
+
+/* Construire M à partir des coordonnées de chaque ville */
+
+
+/* Construire la liste des permutations possibles */
+
+
+/* Evaluer le score de chaque permutations */
+
+
+/* Renvoyer la permutation avec le meilleur score */
+
+
+
+/* ****************** Méthode Elastique *********************** */
