bfce331ad47d61cf769051bbf4c3f44acee49169
lachhebo <ismaellachheb@gmail.com>
2017-12-17 12:47:02 +0100

    creation_matrice

--- dm_tsp.pl
+++ dm_tsp.pl
@@ -17,10 +17,52 @@ dist(a,c,3).
 dist(c,b,9).
 
 
+/* ***************** Production de la matrice à partir des données ******************* */
+
+/* Lecture du fichier  */
+/* lecture des villes dans un fichier tsplib */
+read_villes(Str) :-
+    (at_end_of_stream(Str) ->
+        close(Str);
+
+    read_string(Str, "\n", "\r", _, String),
+    write('Nouvelle ville  = '),writeln(String),
+    split_string(String," ", " ", L),
+    maplist(number_string,LN,L),
+    FV=..[ville|LN],
+    assertz(FV), /* on stocke toutes les coordonnées des villes dans un prédicat dynamique ville(V,XV,YV */
+    read_villes(Str)).
+
+/* read_info_villes(+Str) ou Str est un Stream */
+/* Lecture du fichier .tsplib */
+
+read_info_villes(Str) :-
+    read_string(Str, "\n", "\r", _, String),
+/*     write('Chaine lue = '),writeln(String), */
+    (sub_string(String,0,_,_,"NODE_COORD_SECTION") -> /*tant qu'on n'est pas sur une ligne de ville, on skippe */
+        read_villes(Str);
+        read_info_villes(Str)).
+
+/* read_tsp_file(+NomFichier) avec NomFichier : chaine de caractères */
+/* Appel read_tsp_file('dji38.tsp') */
+
+read_tsp_file(NomF) :-
+    open(NomF,read,Str),
+    retractall(ville(_,_,_)),
+    read_info_villes(Str).
+
+/* Fonction distance */
+
+distance(ville(X1,Y1,Z1),ville(X2,Y2,Z2), D) :- M is sin(Y1)*sin(Y2)+cos(Y1)*cos(Y2)*cos(Z1-Z2), M2 is acos(M), D=[X1,X2,M2].
+
+matrice(M,C,F):- ville(X,Y,Z), ville(X2,Y2,Z2), not(X=X2), verifier([X,X2],C), Cnew=[[X,X2]|C], distance(ville(X,Y,Z),ville(X2,Y2,Z2),D), Mnew=[D|M], matrice(Mnew,Cnew,F),!.
+matrice(M,_,F):- F = M.
+
 /* ****************** Méthode Gloutonne *********************** */
 
 /* Code test :
   glouton(a,[[a,b,4], [c,a,3], [b,c,9], [b,a,4], [a,c,3], [c,b,9]],[],0).
+  glouton(D):- Mat is matrice(M,_,F), glouton(D,Mat,[],0).
 */
 
 min([[A,F]|Q],E,N):- min(Q,A,F,E,N).
@@ -32,10 +74,11 @@ verifier(Tbis,C) :- not(member(Tbis,C)).
 
 /* La méthode doit toujours aller dans la ville qu'il n'a pas encore visité la plus proche de lui jusq'à avoir visité toutes les villes. */
 
+glouton(D):- matrice([],[],F), glouton(D,F,[],0).
 glouton(A,[[M1,M2,M3]|Q],L,C):- distmin(A,[[M1,M2,M3]|Q],[],E,N,L),NewC is E+C, NewL=[A|L], glouton(N,[[M1,M2,M3]|Q],NewL,NewC),!.
 
 /*Lorsque on a fini de parcourir le graphe */
-glouton(A,[[_,_,_]|_],L,C):- NewL=[A|L],write(NewL), write( C).
+glouton(A,[[_,_,_]|_],L,C):- NewL=[A|L],write(NewL), write(" cout total ="), write( C).
 
 distmin(A,[[M,F,D]|Q],C,E,N,L):- M=A,verifier(F,L),distmin(A,Q,[[D,F]|C],E,N,L),!.
 distmin(A,[[_,_,_]|Q],R,E,N,L):- distmin(A,Q,R,E,N,L),!.
